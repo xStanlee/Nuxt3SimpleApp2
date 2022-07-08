12d40cd54fa4705c0df3d8f0228bfac16d32b048
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2022-07-08 08:38:48 +0200

    Add files via upload

---
+++ components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <NuxtLink class="navbar-brand" to="/">Restaurantly</NuxtLink>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 ml-auto">
+                <li class="nav-item">
+                    <NuxtLink class="nav-link active" aria-current="page" to="/">Home</NuxtLink>
+                </li>
+                <li class="nav-item">
+                    <NuxtLink class="nav-link" to="/restaurants">Restaurants</NuxtLink>
+                </li>              
+            </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.ml-auto {
+    margin-left: auto;
+}
+</style>
