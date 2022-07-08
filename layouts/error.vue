6c5381bf4808611276eff048a85d2d5a7d9fd9bb
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2022-07-08 08:41:20 +0200

    Layouts

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <slot name="header" />
+         <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqfKhQ5AI4L3u9sJr7QRZUpOIcyS4Ok2kAmQ&usqp=CAU" alt="Sadface ;(">
+        </div>
+        <slot name="redirect" />
+    </div>
+</template>
+
+<style scoped>
+img {
+  width: 10rem;
+  height: 10rem;
+  margin: 3rem 0;
+}
+</style>
