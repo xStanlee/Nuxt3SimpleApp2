1c218cf5913e00c769821114f05245ee939bc779
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2022-07-08 08:44:27 +0200

    restaurant subPages

---
+++ pages/restaurants/[restaurantName].vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+    import restaurants from '@/data.json';
+
+    const route = useRoute();
+    const restaurantName = route.params.restaurantName ?? 'There\'s no restaurant like this';
+    const restaurant = restaurants.find(({ name }) => name === restaurantName);
+        
+    useHead({
+      title: restaurant ? restaurantName as string : "404 - Restaurant Not Found",
+      meta: [ 
+        {
+        name: "viewport",
+        content: "width=device-width"
+        }
+      ]
+    })
+    const { imageUrl } = restaurant || {};
+    const { name } = restaurant || {};
+    const { revenue } = restaurant || {};
+    const { numberOfStores } = restaurant || {};
+    const { content } = restaurant || {};
+
+</script>
+
+<template>
+  <div>
+    <NuxtLayout name="custom" v-if="restaurant">
+      <div class="restaurant-container">
+        <div class="image-container">
+          <img :src="imageUrl" :alt="`${name} Photo/'s`" />
+        </div>
+        <div class="info-container">
+          <h1>{{ name }}</h1>
+          <div class="stats-container">
+            <h5>Revenue (in billions)</h5>
+            <p>${{ revenue }}</p>
+          </div>
+          <div class="stats-container">
+            <h5>Number of Stores</h5>
+            <p>{{ numberOfStores }}</p>
+          </div>
+          <p class="content">{{ content }}</p>
+        </div>
+      </div>
+    </NuxtLayout>
+    <div v-else class="restaurant-not-found">
+      <NuxtLayout name="error">
+        <template #header>
+          <h1>Restaurant not found</h1>
+        </template>
+        <template #redirect>
+          <button
+            class="btn btn-primary btn-lg"
+            @click="$router.push('/restaurants')">
+          Go back
+          </button>
+        </template>
+      </NuxtLayout>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.restaurant-container {
+  display: flex;
+}
+.image-container {
+  width: 75%;
+  height: 95vh;
+}
+.image-container img {
+  width: 100%;
+  height: 100%;
+}
+.restaurant-not-found {
+  height: 75vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.info-container {
+  padding: 3rem;
+  width: 50%;
+}
+.info-container h1 {
+  text-transform: uppercase;
+  font-size: 6rem;
+  margin-bottom: 3rem;
+}
+.stats-container {
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 1rem;
+}
+.stats-container h5 {
+  width: 20rem;
+  font-size: 2rem;
+  margin: 0;
+  margin-right: 5rem;
+}
+.stats-container p {
+  font-size: 2rem;
+  margin: 0;
+}
+.content {
+  font-size: 1.5rem;
+  margin-top: 4rem;
+}
+</style>
---
+++ pages/restaurants/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Html lang="en-US">
+            <Head>
+            <Title>Top 50 restaurants</Title>
+            <Meta 
+            name="description"
+            content="Learn the stuff"/>
+            <Link rel="preload" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqfKhQ5AI4L3u9sJr7QRZUpOIcyS4Ok2kAmQ&usqp=CAU" as="script" />
+            </Head>
+        </Html>
+        <NuxtLayout name="custom">
+            <div class="container">
+                <RestaurantTable />
+            </div>
+        </NuxtLayout>
+    </div>
+</template>
