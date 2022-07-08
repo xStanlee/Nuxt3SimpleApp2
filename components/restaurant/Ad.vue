435cb2e5ae0b308949983e8a1d0d54f01fa7e895
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2022-07-08 08:40:04 +0200

    components

---
+++ components/restaurant/Ad.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="add-container">
+        <h1>Get the best restaurant guide</h1>
+        <button class="btn btn-primary btn-large">Buy now</button>
+    </div>
+</template>
+
+<style scoped>
+.add-container {
+    margin: 10rem 0;
+    text-align: center;
+    font-size: 2rem;
+    background-color: rgba(103, 103, 231, .3);
+    padding: 3rem;
+}
+</style>
