2d8b006be44c164c5daa520edfea34f4cf93e1f7
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2022-07-08 08:42:07 +0200

    main pages

---
+++ pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mt-5"> 
+    <NuxtLayout name="error">
+      <template #header>
+        <h1>Page not found</h1>
+      </template>
+      <template #redirect>
+        <NuxtLink to="/restaurants">Go back</NuxtLink>
+      </template>
+    </NuxtLayout>
+  </div>
+
+</template>
+<style scoped>
+.container {
+  text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Html lang="en-US">
+      <Head>
+        <Title>Restaurantly</Title>
+        <Meta 
+        name="description"
+        content="Learn the stuff"/>
+        <Link rel="preload" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqfKhQ5AI4L3u9sJr7QRZUpOIcyS4Ok2kAmQ&usqp=CAU" as="script" />
+      </Head>
+    </Html>
+    <div class="container mt-5"> 
+      <h1> Welcome to Restaurantly </h1>
+      <NuxtLink to="/restaurants">Go to restaurants</NuxtLink>
+    </div>
+  </div>
+</template>
+<style scoped>
+.container {
+  text-align: center;
+}
+</style>
